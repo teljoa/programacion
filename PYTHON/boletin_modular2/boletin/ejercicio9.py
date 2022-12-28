7e63ba395219802c3e9b3371a1ac7ab6eac66f00
Joaquín García Téllez <102540516+teljoa@users.noreply.github.com>
2022-12-28 17:54:16 +0100

    Delete PYTHON directory

--- PYTHON/boletin_modular2/boletin/ejercicio9.py
+++
@@ -1,5 +0,0 @@
-'''
-9. Design a method called getPrimeDivisors that receives a positive number as a
-parameter and returns a list containing its prime divisors. If the parameter is not valid
-the method should return None.
-'''
