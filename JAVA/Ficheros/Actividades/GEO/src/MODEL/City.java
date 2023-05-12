6452068a894c32c721f042b2a1231adb095da8d0
Joaquín García Téllez <102540516+teljoa@users.noreply.github.com>
2023-05-12 22:12:37 +0200

    Add files via upload

---
+++ JAVA/Ficheros/Actividades/GEO/src/MODEL/City.java
@@ -0,0 +1,39 @@
+package MODEL;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class City {
+	private int city_id;
+	private String city;
+	private List<Address> addresses;
+	private int country_id;
+	
+	public City(int city_id,String city, int country_id) {
+		this.city_id=city_id;
+		this.city=city;
+		this.country_id=country_id;
+		this.addresses = new ArrayList<>();
+	}
+	
+	public String getCity() {
+		return this.city;
+	}
+	
+	public int getCountryId() {
+		return country_id;
+	}
+	
+	public void addAddress(Address address) {
+		this.addresses.add(address);
+	}
+	
+	public int numeroDeDirecciones() {
+		return this.addresses.size();
+	}
+
+	@Override
+	public String toString() {
+		return " -"+city_id + " "+this.city+" \n"  ;
+	}
+}
